dc21bb15874decfce5c849b9b73ecb703b6e7db2
David Kurz <dk@nevoxo.com>
2016-03-21 17:19:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+PrimeCaution
+===========================================
+
+Diese Erweiterung zeigt eine Warnmeldung sofern ein als "Prime" gekennzeichnetes Produkt nicht durch Amazon versandt wird.
+
+Chrome Webstore: https://chrome.google.com/webstore/detail/primecaution/dfdpdaekhdhnbehdhgjcbnfbondbmlga
---
+++ src/PrimeCaution.js
@@ -0,0 +1,25 @@
+function matchRuleShort(str, rule) {
+  rule = rule.replace(/[*]/g, ".*").toLowerCase().trim();
+  console.log(str+" - "+rule);
+  return new RegExp(rule, "i").test(str);
+}
+
+function isPrime(){
+  return (document.getElementsByClassName('a-icon-prime').length>0);
+}
+
+function isFBA(){
+  return (matchRuleShort(document.getElementById('merchant-info').innerHTML, "Versand durch Amazon"));
+}
+
+(function() {
+  
+  if(!isFBA() && isPrime()){
+    
+    var imgURL = chrome.extension.getURL("prime_reseller.png");
+
+    document.getElementsByClassName('a-icon-prime')[0].style.display= "none";
+    document.getElementById('ourprice_shippingmessage').innerHTML = "<img height='16' src=\'"+imgURL+"\' />";
+  }
+
+})();
